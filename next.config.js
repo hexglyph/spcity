c3b7b3f5ed781aa33b58400376cc012b1b3b6a20
hexglyph <daniel.niebraz@gmail.com>
2022-05-04 18:35:42 -0300

    Ajuste google maps

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable arrow-body-style */
+/* eslint-disable max-len */
 /* eslint-disable import/no-extraneous-dependencies */
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
