4ecb711f85eb7199a74c287e9bb2ab49ab448a56
hexglyph <daniel.niebraz@gmail.com>
2022-05-04 21:31:08 -0300

    Ajuste de Localização e Grid

--- public/assets/js/googlemaps.js
+++ public/assets/js/googlemaps.js
@@ -1,29 +1,30 @@
 /* eslint-disable no-shadow */
-/* eslint-disable no-use-before-define */
 /* eslint-disable linebreak-style */
 /* eslint-disable object-shorthand */
 /* eslint-disable no-undef */
 /* eslint-disable max-len */
 /* eslint-disable indent */
 // Create the Google Map
-      let map;
-      let infoWindow;
+let infoWindow;
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? 'Error: The Geolocation service failed.'
+      : "Error: Your browser doesn't support geolocation.",
+  );
+  infoWindow.open(map);
+}
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 6,
-  });
-  infoWindow = new google.maps.InfoWindow();
-
-  const locationButton = document.createElement('button');
-
-  locationButton.textContent = 'Pan to Current Location';
-  locationButton.classList.add('custom-map-control-button');
-  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
-  // locationButton.addEventListener('click', () => {
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
+      const map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 51.52086, lng: -0.195499 },
+        zoom: 13,
+        mapTypeId: 'roadmap',
+      });
+      infoWindow = new google.maps.InfoWindow();
+      if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
           const pos = {
@@ -44,17 +45,39 @@ function initMap() {
       // Browser doesn't support Geolocation
       handleLocationError(false, infoWindow, map.getCenter());
     }
-  // });
-}
+      let markers = [];
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(
-    browserHasGeolocation
-      ? 'Error: The Geolocation service failed.'
-      : "Error: Your browser doesn't support geolocation.",
-  );
-  infoWindow.open(map);
-}
+      const autosuggest = document.getElementById('autosuggest');
+      autosuggest.addEventListener('selected_suggestion', (value) => {
+        // console.log('[EVENT:select]', value.detail.suggestion.words);
 
-window.initMap = initMap;
+        // Call the what3words convert to coordinates API to obtain the latitude and longitude of the three word address provided
+        what3words.api.convertToCoordinates(value.detail.suggestion.words).then((response) => {
+          // console.log('[convertToCoordinates]', response);
+          if (response.coordinates) {
+            // Clear out the old markers.
+            markers.forEach((marker) => {
+              marker.setMap(null);
+            });
+            markers = [];
+
+            const latLng = { lat: response.coordinates.lat, lng: response.coordinates.lng };
+
+            // Create a marker for the location
+            const marker = new google.maps.Marker({
+              position: latLng,
+              map: map,
+              title: value.detail.suggestion.words,
+              icon: 'https://map.what3words.com/map/marker.png',
+            });
+            markers.push(marker);
+
+            // Center the map on that location, and zoom in on it
+            map.setCenter(latLng);
+            map.setZoom(20);
+          }
+        });
+      });
+    }
+
+initMap();
