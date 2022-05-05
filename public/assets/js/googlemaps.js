a62a4f026262c6d6e26f675f7d5bd29068d0f79b
hexglyph <daniel.niebraz@gmail.com>
2022-05-04 23:24:42 -0300

    Problema no Maps

--- public/assets/js/googlemaps.js
+++ public/assets/js/googlemaps.js
@@ -58,3 +58,27 @@ var markers = [];
           }
         });
       });
+
+const infoWindow = new google.maps.InfoWindow();
+
+if (navigator.geolocation) {
+navigator.geolocation.getCurrentPosition(
+  (position) => {
+    const pos = {
+      lat: position.coords.latitude,
+      lng: position.coords.longitude,
+    };
+
+    infoWindow.setPosition(pos);
+    infoWindow.setContent('Location found.');
+    infoWindow.open(map);
+    map.setCenter(pos);
+  },
+  () => {
+    handleLocationError(true, infoWindow, map.getCenter());
+  },
+);
+} else {
+// Browser doesn't support Geolocation
+handleLocationError(false, infoWindow, map.getCenter());
+}
