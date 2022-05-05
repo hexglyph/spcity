ea7f7c8aaf69f58ed62f2e7fa8135a3e2fc9cb08
hexglyph <daniel.niebraz@gmail.com>
2022-05-04 23:05:59 -0300

    Problema no Maps

--- public/assets/js/googlemaps.js
+++ public/assets/js/googlemaps.js
@@ -1,3 +1,6 @@
+/* eslint-disable vars-on-top */
+/* eslint-disable no-var */
+/* eslint-disable comma-dangle */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-use-before-define */
 /* eslint-disable no-shadow */
@@ -17,42 +20,41 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
   infoWindow.open(map);
 }
 
-const map = new google.maps.Map(document.getElementById('map'), {
-  center: { lat: 51.52086, lng: -0.195499 },
-  zoom: 13,
-  mapTypeId: 'roadmap',
-});
-
-let markers = [];
-
-const autosuggest = document.getElementById('autosuggest');
-autosuggest.addEventListener('selected_suggestion', (value) => {
-  // console.log('[EVENT:select]', value.detail.suggestion.words);
-
-  // Call the what3words convert to coordinates API to obtain the latitude and longitude of the three word address provided
-  what3words.api.convertToCoordinates(value.detail.suggestion.words).then((response) => {
-    // console.log('[convertToCoordinates]', response);
-    if (response.coordinates) {
-      // Clear out the old markers.
-      markers.forEach((marker) => {
-        marker.setMap(null);
+let map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 51.52086, lng: -0.195499 },
+        zoom: 19,
       });
-      markers = [];
 
-      const latLng = { lat: response.coordinates.lat, lng: response.coordinates.lng };
+var markers = [];
 
-      // Create a marker for the location
-      const marker = new google.maps.Marker({
-        position: latLng,
-        map: map,
-        title: value.detail.suggestion.words,
-        icon: 'https://map.what3words.com/map/marker.png',
+      const autosuggest = document.getElementById('autosuggest');
+      autosuggest.addEventListener('selected_suggestion', (value) => {
+        console.log('[EVENT:select]', value.detail.suggestion.words);
+
+        // Call the what3words convert to coordinates API to obtain the latitude and longitude of the three word address provided
+        what3words.api.convertToCoordinates(value.detail.suggestion.words).then((response) => {
+          console.log('[convertToCoordinates]', response);
+          if (response.coordinates) {
+            // Clear out the old markers.
+            markers.forEach((marker) => {
+              marker.setMap(null);
+            });
+            markers = [];
+
+            var latLng = { lat: response.coordinates.lat, lng: response.coordinates.lng };
+
+            // Create a marker for the location
+            var marker = new google.maps.Marker({
+              position: latLng,
+              map: map,
+              title: value.detail.suggestion.words,
+              icon: 'https://map.what3words.com/map/marker.png'
+            });
+            markers.push(marker);
+
+            // Center the map on that location, and zoom in on it
+            map.setCenter(latLng);
+            map.setZoom(20);
+          }
+        });
       });
-      markers.push(marker);
-
-      // Center the map on that location, and zoom in on it
-      map.setCenter(latLng);
-      map.setZoom(20);
-    }
-  });
-});
