d9af225b6e354a1c8eccaac62beffc31780a1375
Daniel Niemietz Braz <dnbraz@prodam.sp.gov.br>
2025-02-22 21:43:08 -0300

    Adicionar novo logo e atualizar componente TopMenu para usar a nova imagem

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -6,7 +6,7 @@ const TopMenu = () => {
         <div className="fixed top-0 left-0 right-0 bg-white shadow-md z-50" data-topmenu>
             <div className="container mx-auto px-4 py-2 flex items-center justify-between">
                 <div className="flex items-center space-x-4">
-                    <Image src="/spcity-logo.svg" alt="SPCity Logo" width={40} height={40} />
+                    <Image src="/logo.png" alt="SPCity Logo" width={60} height={40} />
                     <div className="flex flex-col">
                         <span className="text-lg font-bold text-gray-800">SPCity</span>
                         <span className="text-sm text-gray-600">Acolhimento de Demandas do Cidadão de São Paulo</span>
